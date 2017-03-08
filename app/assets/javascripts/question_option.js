7f6629279698f9f98ecd055eb2a74f797a81bf4d
JonasRenngård <jonas.aleksander95@gmail.com>
2017-03-08 16:53:15 +0100

    create quiz

---
+++ app/assets/javascripts/question_option.js
@@ -0,0 +1,28 @@
+$(document).on('turbolinks:load', function() {
+  $(".question-option").on("click", function(event) {
+
+    var option = $(event.target);
+    var options = $(event.target.parentElement.children);
+
+    if (option.hasClass("btn-yellow")) {
+      options.removeClass("btn-secondary");
+      options.addClass("btn-yellow");
+      option.toggleClass("btn-secondary");
+      option.removeClass("btn-yellow");
+    }
+
+    else if (option.hasClass("btn-secondary")) {
+      option.removeClass("btn-secondary");
+      option.toggleClass("btn-yellow");
+    }
+  });
+
+  $(".submit-quiz").on("click", function(event) {
+    var right_answers = $(".ro");
+    var wrong_answers = $(".btn-secondary"); 
+    right_answers.removeClass("btn-yellow");
+    right_answers.addClass("btn-success");
+    wrong_answers.removeClass("btn-secondary");
+    wrong_answers.addClass("btn-alert");
+  });
+});
--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find params[:id]
     @trivium = @activity.trivia
-    @questions = @activity.questions
+    @questions = @activity.questions.order("RANDOM()").first(3)
     @images = @activity.activity_images
     @activity_image = ActivityImage.new
   end
