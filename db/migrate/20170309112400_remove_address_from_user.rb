6512d60aa7d7b902e203e8bad355a196df60380d
kallelundgren93 <kallelundgren93@users.noreply.github.com>
2017-03-09 13:42:42 +0100

    changed address to triproute instead of user (#29)

---
+++ db/migrate/20170309112400_remove_address_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveAddressFromUser < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :users, :address
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309081344) do
+ActiveRecord::Schema.define(version: 20170309112855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(version: 20170309081344) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "active",     default: false, null: false
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_trip_routes_on_user_id", using: :btree
   end
 
@@ -113,7 +116,6 @@ ActiveRecord::Schema.define(version: 20170309081344) do
     t.datetime "avatar_updated_at"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "address"
     t.string   "first_name",             default: "", null: false
     t.string   "surname",                default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
