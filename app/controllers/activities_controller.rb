438fc678693aa0d49ef8ab42f6ee39129bfcddc9
kallelundgren93 <Lunkenn@gmail.com>
2017-03-10 15:19:06 +0100

    pagination

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
     @activity = Activity.find params[:id]
     @trivium = @activity.trivia
     @questions = @activity.questions.order("RANDOM()").first(3)
-    @images = @activity.activity_images
+    @images = @activity.activity_images.paginate(:page => params[:page], :per_page => 6)
     @activity_image = ActivityImage.new
     @feedbacks = @activity.feedbacks.first_four_by_date
     @feedback = Feedback.new
