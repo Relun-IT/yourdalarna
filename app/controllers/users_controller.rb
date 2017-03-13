8409c982c62889ea7ff64ac6b639eedc9704d93c
Jonas Renngård <h14jonre@du.se>
2017-03-13 09:42:09 +0100

    Bugfixes (#45)
    * fix users show bugs
    
    * add title to anonymous reviews
    
    * add anonymous title to images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @trip_route = TripRoute.new
-    @trip_routes = current_user.trip_routes.by_date.paginate(:page => params[:page], :per_page => 20)
-    @images = current_user.activity_images.paginate(:page => params[:page], :per_page => 8)
-    @latest_trip_route = current_user.trip_routes.latest
-    @activities = @latest_trip_route.trip_route_activities.top_three
+    @trip_routes = @user.trip_routes.by_date.paginate(:page => params[:page], :per_page => 20)
+    @images = @user.activity_images.paginate(:page => params[:page], :per_page => 8)
+    @latest_trip_route = @user.trip_routes.latest
+
+    if @latest_trip_route.present?
+      @activities = @latest_trip_route.trip_route_activities.top_three
+    end
+
     @events = Event.by_date.paginate(:page => params[:page], :per_page => 3)
   end
 
