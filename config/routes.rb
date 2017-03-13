76f7bbb005de8055938dfe3952854bfcb554bbe5
JonasRenngård <jonas.aleksander95@gmail.com>
2017-03-13 09:57:19 +0100

    users can now remove their own feedback

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :feedbacks, only: [:new, :create]
   end
   resources :activity_images, only: [:destroy]
+  resources :feedbacks, only: [:destroy]
   resources :trip_routes, only: [:show, :update, :new, :create] do
     resources :trip_route_activities, only: [:new, :create, :destroy]
   end
